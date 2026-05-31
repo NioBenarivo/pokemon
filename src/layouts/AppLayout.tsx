d0be0db09c73693b89e815a1061aeb96ada0a5d4
zunio <zunibenarivo@gmail.com>
2026-05-31 15:04:04 +0700

    feat: add scroll to top

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import NavBar from '../components/NavBar'
+import ScrollToTop from '../components/ScrollToTop'
 
 export default function AppLayout() {
   return (
     <div className="min-h-screen pb-20">
+      <ScrollToTop />
       <Outlet />
       <NavBar />
     </div>
